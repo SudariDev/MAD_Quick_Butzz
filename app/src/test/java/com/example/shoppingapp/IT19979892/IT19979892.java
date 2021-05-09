2c7aa94a8c4f3bb45e66204092726c837e9109fb
ashanq <maduwanthaashan230@gmail.com>
2021-05-09 15:31:22 +0530

    added admin dashboard

---
+++ app/src/test/java/com/example/shoppingapp/IT19979892/IT19979892.java
@@ -0,0 +1,68 @@
+package com.example.shoppingapp.IT19979892;
+
+import com.example.shoppingapp.Check_Stock;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class IT19979892 {
+
+    private static Check_Stock check_stock;
+    int result;
+
+
+
+    @BeforeClass
+    public static void initCheck_stock()
+    {
+        check_stock = new Check_Stock();
+    }
+    @Before
+    public void setUp()
+    {
+        result = 0;
+
+    }
+
+    @Test
+    public void TestMultiplication (){
+
+       result = check_stock.totPrice(600,2);
+       assertEquals(1200,result);
+
+
+       }
+
+    @Test
+    public void TestMultiplication2 (){
+
+        result = check_stock.totPrice(800,0);
+        assertEquals(0,result);
+    }
+
+    @After
+
+    public void clearData(){
+        result = 0;
+
+    }
+
+    @AfterClass
+    public  static void clearAll()
+    {
+        check_stock = null;
+
+    }
+}
+//test
+//IT19979892
